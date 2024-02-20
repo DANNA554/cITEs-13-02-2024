352a4529c8e1e72c4bb1be8415504e3bf18f1dc6
sev7n <103088037+svn20@users.noreply.github.com>
2024-02-19 17:46:38 -0800

    Update SqlCitas.java

--- cITEs/src/main/java/database/SqlCitas.java
+++ cITEs/src/main/java/database/SqlCitas.java
@@ -16,13 +16,43 @@ import java.sql.SQLException;
  */
 public class SqlCitas extends SqlConector {
     public void crearCita(Cita cita){ }
+
+
     
     public Cita[] consultarCitasPorAlumno(int idAlumno){
+        Cita citaAlumno = new Cita();
+        String sqlSelect = "SELECT * FROM Alumnos WHERE correo = (?) AND contrasenia = (?)";
+
+        
+        try {
+            PreparedStatement PS = this.getConnection().prepareStatement(sqlSelect);
+            RS = PS.executeQuery();
+            
+            // Agregado para la base de datos 19/02/2024
+            citaAlumno.setId_cita(RS.getString(1)); 
+            citaAlumno.setFecha( RS.getString(2));
+            citaAlumno.setHora( Integer.parseInt(RS.getString(3)));
+            citaAlumno.setCitadorId(RS.getString(4));
+            citaAlumno.setCitadorDocente(RS.getString(5));
+            citaAlumno.setMotivo(RS.getString(4));
+            citaAlumno.setEstado(RS.getString(5));
+            
+            //Desconosco si esto se rompera por que el metodo esta dise;ado para agregar a listas
+        } catch(Exception e){
+            System.out.println("Error al modificar datos de alumno: "+e.getMessage());
+            
+        } finally{
+            this.desconectar();
+            this.close();
+            return alumno;
+        }
+    }
         return 
     }
     
     
     public Cita[] consultarCitasPorCoordinador(int idCoordinador){
+        
         return
     }
     
